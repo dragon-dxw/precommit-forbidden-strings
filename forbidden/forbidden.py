cfa4e36d279decf76314828f7a3351818403f1d7
David McKee <david.mckee@dxw.com>
2023-08-04 11:15:55 +0100

    better messages

--- forbidden/forbidden.py
+++ forbidden/forbidden.py
@@ -7,12 +7,13 @@ def start():
   filenames = sys.argv[2:]
   with open(forbidden_file, "r") as f:
     forbidden_words = [x.strip() for x in f.readlines()]
+  print(f"{len(forbidden_words)} found")
   for checked_filename in filenames:
     with open(checked_filename, "r") as f:
-      file_contents = f.read()
-      for forbidden_word in forbidden_words:
+      file_contents = f.readlines()
+      for line, forbidden_word in enumerate(forbidden_words):
         if forbidden_word in file_contents:
-          print(f"forbidden word '{forbidden_word}' found in {checked_filename}")
+          print(f"forbidden word '{forbidden_word}' found in {checked_filename} on line {line+1}")
           fail = 1
   exit(fail)
 if __name__ == "__main__":
